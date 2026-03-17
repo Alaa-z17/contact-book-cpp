daed1ee56080e4283b2c2485248c51a70788f05e
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-17 05:20:57 +0300

    feat: add display and print functions

--- contact-book-cpp/contact-book-cpp.cpp
+++ contact-book-cpp/contact-book-cpp.cpp
@@ -27,6 +27,53 @@ struct stContact
     string Email = "";
     string Address = "";
 };
+string FullName(stContact Contact)
+{
+    return Contact.FirstName + " " + Contact.LastName;
+}
+
+void PrintContactCard(stContact &Contact)
+{
+    cout << "\n";
+    cout << string(40, '-') << "\n";
+    cout << left << setw(12) << "Name"
+        << ": " << FullName(Contact) << "\n";
+    cout << left << setw(12) << "Phone"
+        << ": " << Contact.Phone << "\n";
+    cout << left << setw(12) << "Email"
+        << ": " << Contact.Email << "\n";
+    cout << left << setw(12) << "Address"
+        << ": " << Contact.Address << "\n";
+    cout << string(40, '-') << "\n";
+}
+
+void PrintContactList(vector<stContact>& vContacts)
+{
+    if (vContacts.empty())
+    {
+        cout << "\nNo Contacts Found!\n";
+        return;
+    }
+
+    cout << "\n";
+    cout << left
+        << setw(5) << "No."
+        << setw(20) << "Name"
+        << setw(15) << "Phone"
+        << setw(25) << "Email" << "\n";
+    cout << string(65, '-') << "\n";
+
+    for (int i = 0; i < vContacts.size(); i++)
+    {
+        cout << left
+            << setw(5) << i + 1
+            << setw(20) << FullName(vContacts[i])
+            << setw(15) << vContacts[i].Phone
+            << setw(25) << vContacts[i].Email << "\n";
+    }
+    cout << string(65, '-') << "\n";
+    cout << "Total Contacts: " << vContacts.size() << "\n";
+}
 
 int main()
 {
